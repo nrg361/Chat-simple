777bdb0223d21f0bde8ba867344bef43d5a403de
Anurag Shrivastava <81407875+nrg361@users.noreply.github.com>
2023-02-18 12:10:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('recieve', { message: message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+
+})
+
+server.listen(process.env.port || 3000)
+
+// npm start
+// npm run devStart
